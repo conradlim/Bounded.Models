a53b1abc5e03d30656b71ff0acf0fdc54b496c73
clim <conradlim.net@yahoo.com>
2021-10-04 23:58:35 -0700

    Add project files.

---
+++ Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bounded.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name="Category Name")]
+        [MaxLength(50)]
+        public string Name{ get; set; }
+    }
+}
